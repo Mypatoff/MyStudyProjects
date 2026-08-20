950c52e777dd09216933ea3ef033f501283f39f1
Davron <cyberac89@gmail.com>
2026-08-20 22:54:00 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hi, I'm Davron</h1><p>This is my website, and it's running on Go.</p>")
+}
 
 func main() {
-	fmt.Println("Main Page")
+	http.HandleFunc("/", homePage)
+	fmt.Println("Server running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
