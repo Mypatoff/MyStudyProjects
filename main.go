79d23fff522a55ce58e1daf403cc8dda673eeee7
Davron <cyberac89@gmail.com>
2026-08-25 19:35:56 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Davron!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
 }
