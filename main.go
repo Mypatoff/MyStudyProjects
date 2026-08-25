2ba14778ce5b970235070dd9ff8e1cf7191b8fa7
Davron <cyberac89@gmail.com>
2026-08-25 19:33:53 +0500

    Starting from a new page

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"net/http"
-)
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("Pages_Frontend/home.html"))
-	tmpl.Execute(w, nil)
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", homePage)
-	fmt.Println("Server running at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}
-func laziness() {
-	fmt.Println("Wtf am I gonna do ?")
-	// I've been wasting all the time
-	// I cannot continue like that.
+	fmt.Println("Hello, World!")
 }
