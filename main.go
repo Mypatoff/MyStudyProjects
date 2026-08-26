ce3df5550ddb0ee44917897e6041c805ef597cde
Davron <cyberac89@gmail.com>
2026-08-26 21:08:53 +0500

    update

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+package main
+
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page.")
+}
 
 func greetPage(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
@@ -19,3 +29,12 @@ func greetPage(w http.ResponseWriter, r *http.Request) {
 
 	tmpl.Execute(w, name)
 }
+
+func main() {
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/greet", greetPage)
+	http.ListenAndServe(":8080", nil)
+}
+
+// we have learnt how to
