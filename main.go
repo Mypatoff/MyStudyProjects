74edfd3a65c29a9ad5763719352e571d2a8962ca
Davron <cyberac89@gmail.com>
2026-08-31 22:11:07 +0500

    another update

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	basicIf(18)
-}
-func basicIf(n int) {
-	if n%2 == 1 {
-		fmt.Println("Odd")
-	} else {
-		fmt.Println("Not odd")
+	// basicIf(18)
+	for i := range 5 {
+		goRoutine(i)
+		time.Sleep(time.Second)
 	}
+	for i := 5; i <= 10; i++ {
+		goRoutine(i)
+		time.Sleep(time.Second)
+	}
+}
+
+//	func basicIf(n int) {
+//		if n%2 == 1 {
+//			fmt.Println("Odd")
+//		} else {
+//			fmt.Println("Not odd")
+//		}
+//	}
+func goRoutine(i int) {
+	fmt.Println(i)
 }
