3fcd220e003577af7393d93451632ff67221d865
Davron <cyberac89@gmail.com>
2026-09-01 19:17:27 +0500

    update tdy

--- main.go
+++ main.go
@@ -2,19 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	// basicIf(18)
-	for i := range 5 {
-		goRoutine(i)
-		time.Sleep(time.Second)
-	}
-	for i := 5; i <= 10; i++ {
-		goRoutine(i)
-		time.Sleep(time.Second)
-	}
+	fmt.Println("For today, I'll rest")
 }
 
 //	func basicIf(n int) {
