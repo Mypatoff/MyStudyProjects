7d2e2d0314feb60db98527e7374165dfee140ca8
Davron <cyberac89@gmail.com>
2026-09-05 19:48:16 +0500

    update new

--- main.go
+++ main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("I have no idea what to do nowadays")
-	//All I am doing is wasting time...
+	fmt.Println("Anything")
 }
-
-//	func basicIf(n int) {
-//		if n%2 == 1 {
-//			fmt.Println("Odd")
-//		} else {
-//			fmt.Println("Not odd")
-//		}
-//	}
-func goRoutine(i int) {
-	fmt.Println(i)
-}
-
-//I totally forgot about my code((
-
-//Why am I searching for some bullshit and looking at em when I have lot to do ??
